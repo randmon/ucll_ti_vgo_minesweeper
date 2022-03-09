a3a9b0ad622220d5ff7aeefcd195488dc1e3873f
randmon <ccmarques98@gmail.com>
2022-03-09 11:45:53 +0100

    converter correct colors hardcoded

--- src/View/Converters/SquareStatusConverter.cs
+++ src/View/Converters/SquareStatusConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.MineSweeper;
+using System;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
@@ -9,7 +10,20 @@ namespace View.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return Brushes.Red;
+            SquareStatus status = (SquareStatus) value;
+            switch (status)
+            {
+                case SquareStatus.Covered:
+                    return Brushes.Transparent;
+                case SquareStatus.Flagged:
+                    return Brushes.Orange;
+                case SquareStatus.Mine:
+                    return Brushes.Black;
+                case SquareStatus.Uncovered:
+                    return Brushes.Transparent;
+                default:
+                    throw new ArgumentException("Invalid square status at converter");
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
