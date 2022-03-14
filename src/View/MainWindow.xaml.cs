acae7cbc2e2a99f75ed3d0775b2dfac60f23c1d6
randmon <ccmarques98@gmail.com>
2022-03-14 14:29:12 +0100

    gameviewmodel

--- src/View/MainWindow.xaml.cs
+++ src/View/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Model.MineSweeper;
 using System.Collections.Generic;
 using System.Windows;
+using ViewModel;
 
 namespace View
 {
@@ -36,28 +37,8 @@ namespace View
             game = game.ToggleFlag(new Vector2D(2, 0));
             game = game.UncoverSquare(new Vector2D(3, 0)); //Uncover with bomb, ends game!
 
-            this.DataContext = Rows(game.Board);
-        }
-
-        public IEnumerable<Square> Row(IGameBoard board, int row)
-        {
-            List<Square> result = new List<Square>();
-            for (int i = 0; i < board.Width; i++)
-            {
-                var position = new Vector2D(i, row);
-                result.Add(board[position]);
-            }
-            return result;
-        }
-
-        public IEnumerable<IEnumerable<Square>> Rows(IGameBoard board)
-        {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-            for (int i = 0; i < board.Height; i++)
-            {
-                result.Add(Row(board, i));
-            }
-            return result;
-        }
+            GameViewModel gameViewModel = new GameViewModel(game);
+            this.DataContext = gameViewModel;
+        }        
     }
 }
---
+++ src/ViewModel/GameViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Model.Data;
+using Model.MineSweeper;
+
+namespace ViewModel
+{
+    public class GameViewModel
+    {
+        private readonly IGame _game;
+        public GameViewModel(IGame game)
+        {
+            _game = game;
+            Rows = GetRows();
+         }
+
+        public IEnumerable<IEnumerable<Square>> Rows { get; }
+
+        public IEnumerable<IEnumerable<Square>> GetRows()
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+            for (int i = 0; i < _game.Board.Height; i++)
+            {
+                result.Add(Row(i));
+            }
+            return result;
+        }
+
+
+        public IEnumerable<Square> Row(int row)
+        {
+            List<Square> result = new List<Square>();
+            for (int i = 0; i < _game.Board.Width; i++)
+            {
+                var position = new Vector2D(i, row);
+                result.Add(_game.Board[position]);
+            }
+            return result;
+        }
+    }
+}
