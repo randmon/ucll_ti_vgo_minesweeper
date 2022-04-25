3ed8e0ac48e5810d0a5d77b59fbf48e43d7d76ec
randmon <ccmarques98@gmail.com>
2022-04-25 16:18:32 +0200

    blank squares

---
+++ src/View/Converters/NeighbouringMinesConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace View.Converters
+{
+    internal class NeighbouringMinesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int mineCount = (int)value;
+            switch (mineCount)
+            {
+                case 0:
+                    return "";
+                default:
+                    return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
